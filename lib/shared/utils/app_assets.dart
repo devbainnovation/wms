2ec48637332b08971c78fc2af30d3b18380730b8
Bhadresh Sorathiya <bhadresh0768@gmail.com>
2026-04-07 16:13:46 +0530

    move spash image to logo folder

--- lib/shared/utils/app_assets.dart
+++ lib/shared/utils/app_assets.dart
@@ -1,7 +1,7 @@
 class AppAssets {
   AppAssets._();
 
-  static const String logo = 'assets/images/devbaa_logo.png';
+  static const String logo = 'assets/images/logo/devbaa_logo.png';
   static const String motor = 'assets/images/icons/iconMotor.png';
   static const String valve = 'assets/images/icons/iconValve.png';
 }
